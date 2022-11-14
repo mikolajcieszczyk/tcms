02e27e79b3325a4b5a69e8b940c7d83e8146e008
mikolajcieszczyk <mikolaj.cieszczyk@gmail.com>
2022-11-14 10:10:23 +0100

    [API] added Courts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
 import { ConfigModule } from '@nestjs/config';
 import { BookingsModule } from './bookings/bookings.module';
+import { CourtsModule } from './courts/courts.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { BookingsModule } from './bookings/bookings.module';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     BookingsModule,
+    CourtsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
