9728d4124ee419316784d89521ed1605669b8b7d
mikolajcieszczyk <mikolaj.cieszczyk@gmail.com>
2022-11-14 09:29:55 +0100

    [API] updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(
+    session({
+      secret: process.env.SECRET_KEY,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
+  });
+  await app.listen(process.env.PORT);
 }
 bootstrap();
